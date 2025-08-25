28f0df22100416480f3e7fdb01dc5c2c7afd977d
1987Shiz321 <ya3ma2za3ki8@gmail.com>
2025-08-25 15:32:42 +0900

    スプレッドオペレータの練習

--- src/app.ts
+++ src/app.ts
@@ -11,3 +11,22 @@ const button = document.querySelector('button')!;
 if (button) {
     button.addEventListener('click', event => console.log(event));
 }
+
+//スプレッド演算子の練習
+const hobbies = ['Sports', 'Cooking'];
+const activeHobbies = ['Hiking'];
+
+// constで定義した配列はpushで要素を追加できる
+activeHobbies.push(...hobbies);
+
+//オブジェクトの定義
+const person = {
+    name: 'Max',
+    age: 30
+};
+
+//オブジェクトのコピー
+const copiedPerson = {
+    ...person
+};
+console.log(copiedPerson);
