b2f6fa661493f692895ed6534c696b51d7b6847f
1987Shiz321 <ya3ma2za3ki8@gmail.com>
2025-08-28 15:33:31 +0900

    継承やオーバーライド,protectedについての学習

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,8 @@
 // クラスの頭文字は大文字で始めるのが慣例
 class Department {
-    // privateはクラスの中でしかアクセスできない
-    private employees: string[] = [];
+    // privateはクラスの中でしかアクセスできないし、継承したクラスからもアクセスできない
+    // protectedはクラスの中と継承したクラスからアクセスできる
+    protected employees: string[] = [];
 
     constructor(private readonly id: string, public name: string) {
         // thisはクラスの中で使う
@@ -27,6 +28,41 @@ class Department {
         console.log(this.employees);
     }
 }
+
+//継承についての学習
+class ITDepartment extends Department {
+    constructor(id: string, private admins: string[]) {
+        super(id, 'IT'); //親クラスのコンストラクタを呼び出す.ITDepartmentクラスのnameプロパティは'IT'に固定している
+        //adminsプロパティはITDepartmentクラスでのみ使用されるので、superで呼び出す必要はない
+        this.admins = admins;
+    }
+}
+
+class AccountingDepartment extends Department {
+    constructor(id: string, private reports: string[]) {
+        super(id, 'Accounting'); //親クラスのコンストラクタを呼び出す.
+        this.reports = reports;
+    }
+    
+    addReport(text: string) {
+        this.reports.push(text);
+    }
+
+    printReports() {
+        console.log(this.reports);
+    }
+
+    // 親クラスのメソッドをオーバーライドする
+    addEmployee(employee: string): void {
+        // 例えば、特定の名前の社員を追加できないようにする
+        if (employee === 'mkwiimatsumoto') {
+            return;
+        }
+        this.employees.push(employee);
+    }
+}
+
+
 // インスタンス化
 // newクラス名()でインスタンス化できる
 // インスタンス化したオブジェクトを変数に格納する
@@ -35,10 +71,20 @@ const accounting = new Department('d1','saino');
 accounting.addEmployee('mkwiimatsumoto');
 accounting.addEmployee('kazuki23650214');
 
-
-
 accounting.describe();
 accounting.printEmployeeInformation();
+
+const it01 = new ITDepartment('d2', ['mkwiimatsumoto']);
+console.log(it01);
+
+const accounting2 = new AccountingDepartment('d3', []);
+accounting2.addReport('Something went wrong...');
+accounting2.printReports();
+
+accounting2.addEmployee('hikakin');
+accounting2.addEmployee('seikin');
+accounting2.addEmployee('mkwiimatsumoto'); //追加されない
+accounting2.printEmployeeInformation();
 
 // const accountingCopy = {
 //     name: 'mkwiimatsumoto',
@@ -47,7 +93,7 @@ accounting.printEmployeeInformation();
 
 // accountingCopy.describe(); //undefinedになる
 
-// ウマ娘の名前と二つ名を格納するクラス
+// 練習.ウマ娘の名前と二つ名を格納するクラス
 class Umamusume {
     private name: string;
     private subtitle: string;
