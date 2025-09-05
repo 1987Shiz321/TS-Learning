131d1bb7c81a38c5bfb5ef081c80a8d4e3c6fd3b
1987Shiz321 <ya3ma2za3ki8@gmail.com>
2025-09-05 15:18:57 +0900

    デコレータ1

--- src/app.ts
+++ src/app.ts
@@ -95,4 +95,28 @@ class Product {
 const p1 = new Product('Book', 200);
 const p2 = new Product('TextBook', 400);
 
-console.log(p1, p2);
+function Autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+    const originalMethod = descriptor.value;
+    const adjDescriptor: PropertyDescriptor = {
+        configurable: true,
+        enumerable: false,
+        get() {
+            const boundFn = originalMethod.bind(this);
+            return boundFn;
+        }
+    }
+    return adjDescriptor;
+}
+class Printer {
+    message = 'クリックしました';
+
+    @Autobind
+    showMessage() {
+        console.log(this.message);
+    }
+}
+
+const p = new Printer();
+
+const button = document.querySelector("button")!;
+button.addEventListener('click', p.showMessage);
