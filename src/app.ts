00bffd3539f3598c857685b88b6c07ef2c99c78e
1987Shiz321 <ya3ma2za3ki8@gmail.com>
2025-09-08 10:32:25 +0900

    128

--- src/app.ts
+++ src/app.ts
@@ -45,6 +45,34 @@ function autobind(
     return adjDescriptor;
 }
 
+// ProjectList Class
+class ProjectList {
+    templateElement: HTMLTemplateElement;
+    hostElement: HTMLDivElement;
+    element: HTMLElement;
+    
+    constructor(private type: 'active' | 'finished') {
+        this.templateElement = document.getElementById('project-list')! as HTMLTemplateElement;
+        this.hostElement = document.getElementById('app')! as HTMLDivElement;
+
+        const importedNode = document.importNode(this.templateElement.content, true);
+        this.element = importedNode.firstElementChild as HTMLElement;
+        this.element.id = `${this.type}-projects`;
+        this.attach();
+        this.renderConent();
+    }
+
+    private renderConent() {
+        const listId = `${this.type}-projects-list`;
+        this.element.querySelector('ul')!.id = listId;
+        this.element.querySelector('h2')!.textContent = this.type === 'active' ? '実行中のプロジェクト' : '完了プロジェクト';
+    }
+
+    private attach() {
+        this.hostElement.insertAdjacentElement('beforeend', this.element);        
+    }
+}
+
 // ProjectInput Class
 class ProjectInput {
     templateElement: HTMLTemplateElement;
@@ -134,4 +162,7 @@ class ProjectInput {
 }
 
 const prjInput = new ProjectInput();
+const activePrjList = new ProjectList('active');
+const finishedPrjList = new ProjectList('finished');
+
 console.log("a");
